8b6de3ebaf65290f2f41e406ed24f104ee52fa26
darwinyara0526 <darwinyara0526@gmail.com>
2022-11-15 10:28:17 -0500

    version3.1

--- app/src/main/java/com/example/lostonmars/ui/GameMainInterface.kt
+++ app/src/main/java/com/example/lostonmars/ui/GameMainInterface.kt
@@ -1,24 +1,31 @@
 package com.example.lostonmars.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.lostonmars.R
-import com.example.lostonmars.SettingActivity
+import com.example.lostonmars.*
 import com.example.lostonmars.game.GameIntro
 
 class GameMainInterface : AppCompatActivity() {
 
     private lateinit var btnJugar: Button
     private lateinit var btnSetting:Button
+    private lateinit var btnLogros1:Button
+    private lateinit var btnNarracion:Button
+    private lateinit var btnReglas:Button
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_main_interface)
 
         btnJugar = findViewById(R.id.btnJugar)
         btnSetting = findViewById(R.id.btnSetting)
+        btnLogros1 = findViewById(R.id.btnLogros1)
+        btnNarracion = findViewById(R.id.btnNarracion)
+        btnReglas = findViewById(R.id.btnReglas)
 
         btnJugar.setOnClickListener {
             val lanzar = Intent(this, GameIntro::class.java)
@@ -26,6 +33,18 @@ class GameMainInterface : AppCompatActivity() {
         }
         btnSetting.setOnClickListener {
             val lanzar = Intent(this, SettingActivity::class.java)
+            startActivity(lanzar)
+        }
+        btnLogros1.setOnClickListener {
+            val lanzar = Intent(this, LogrosActivity::class.java)
+            startActivity(lanzar)
+        }
+        btnNarracion.setOnClickListener {
+            val lanzar = Intent(this, NarraccionActivity::class.java)
+            startActivity(lanzar)
+        }
+        btnReglas.setOnClickListener {
+            val lanzar = Intent(this, ReglasActivity::class.java)
             startActivity(lanzar)
         }
     }
